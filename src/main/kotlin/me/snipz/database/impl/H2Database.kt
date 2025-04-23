658be639abfd51eb678ae39167c64275c3747940
devoirr <138160506+devoirr@users.noreply.github.com>
2025-04-24 02:45:27 +0300

    Fixed H2 driver problem.

--- src/main/kotlin/me/snipz/database/impl/H2Database.kt
+++ src/main/kotlin/me/snipz/database/impl/H2Database.kt
@@ -6,6 +6,10 @@ import java.sql.DriverManager
 
 class H2Database(private val path: String): GeneralDatabase() {
 
+    init {
+        Class.forName("org.h2.Driver")
+    }
+
     override fun getConnection(): Connection {
         val connection = DriverManager.getConnection("jdbc:h2:$path;MODE=MySQL")
         return connection
